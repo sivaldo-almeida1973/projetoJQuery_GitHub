a73331af7d905dd25aabfadaac1b3bee0fc953ea
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2024-01-04 20:35:20 -0300

    criada a pagina sequencia_execucao.html/script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    
+  console.log($('#exemplo'))
+
+
+
+})
+
+
+//as duas formas iraõ funcionar igualmente
+
+/*
+function teste() {
+  console.log($('#exemplo'))
+}
+
+$(teste)
+
+*/
